e72176bb2cdb2e1f86b06a68c60785b47ea956fa
SeJongDeveloper <51711799+sejongdeveloper@users.noreply.github.com>
2020-03-03 23:32:00 +0900

    c언어 1일차

---
+++ c언어/ch1/Hello.c
@@ -0,0 +1,9 @@
+/*	예제 : Hello 출력하기 
+	파일 이름 : Hello.c
+	수정 일자 : 2020년 03월 03일
+	작성자 : 김세종 
+*/
+#include <stdio.h> 			// 헤더 파일 추가
+void main() { 				// main 함수 시작 
+	printf("Hello~ \n"); 	// 모니터에 문자열 출력 
+} 							// main 함수 끝 
---
+++ c언어/ch1/sample.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void main()
+{
+	printf("hello C 프로그래밍 \n");
+}
